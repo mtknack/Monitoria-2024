aa16b2c523e955a4553f2f128d58301abf820588
mtknack-itixpc <mtknack555@gmail.com>
2024-04-18 18:45:29 -0300

    5

---
+++ 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 5. Faça um programa em C que leia dois números reais e os imprima.
+void main(){
+
+    float n1, n2;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &n1);
+    printf("Digite o segundo numero: ");
+    scanf("%f", &n2);
+
+    printf("Numero 1 e: %f e numnero 2 e: %f\n", n1, n2);
+}
