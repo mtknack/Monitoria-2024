028f8f1a03eca4d0cb7c75b9cecb499dc4c14424
laiscost <167798785+laiscost@users.noreply.github.com>
2024-05-20 17:51:05 -0300

    1.c

---
+++ Capítulo 3/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Loop de 1 até 100
+    for (int num = 1; num <= 100; num++) {
+        printf("%d\n", num); // Imprime o número atual
+    }
+    
+    return 0;
+}
