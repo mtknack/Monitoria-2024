b2e163e30cdd64d07f370f3ff410220e411975d4
laratidesco <54151256+laratidesco@users.noreply.github.com>
2024-05-22 18:39:18 -0300

    2.c
    correção (havia copiado o exercicio 3 na numero 2)

--- Capítulo 3/2.c
+++ Capítulo 3/2.c
@@ -1,10 +1,10 @@
-/* 3. Faça um programa que imprima os múltiplos de 5, no intervalo de 1 até 500. */
+/* 2. Faça um programa que imprima todos os números pares de 100 até 1. */
 
 #include <stdio.h>
 
 int main(void) {
-  for(int i = 1; i <= 500; i++){
-    if(i % 5 == 0){
+  for(int i = 100; i > 0; i--){
+    if(i % 2 == 0){
       printf("%d\n", i);
     }
   }
