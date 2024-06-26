6915d9227740daf00ff56d6ab425328e93859c21
laratidesco <54151256+laratidesco@users.noreply.github.com>
2024-06-25 22:40:42 -0300

    12.c

--- Capítulo 5/12.c
+++ Capítulo 5/12.c
@@ -1,4 +1,4 @@
-/* 12. Uma floricultura conhecedora de sua clientela gostaria de fazer um programa que pudesse controlar sempre um estoque mínimo de determinadas plantas, pois todo dia, pela manhã, o dono faz novas aquisições. Criar um algoritmo que deixe cadastrar 50 plantas, nome, estoque mínimo, estoque atual. Imprimir ao final do programa uma lista das plantas que devem ser adquiridas. */
+/* 12. Uma floricultura conhecedora de sua clientela gostaria de fazer um programa que pudesse controlar sempre um estoque mínimo de determinadas plantas, pois todo dia, pela manhã, o dono faz novas aquisições. Criar um algoritmo que deixe cadastrar 50 plantas, nome, estoque mínimo, estoque atual. Imprimir ao final do programa uma lista das plantas que devem ser adquiridas.  */
 
 #include <stdio.h>
 #include <string.h>
@@ -16,7 +16,8 @@ int main(void) {
     printf("Cadastro da planta %d:\n", i + 1);
     
     printf("\nNome da planta: ");
-    fgets(nomes[i], 100, stdin);
+    fgets(nomes[i], 50, stdin);
+    
     // Remover o caractere de nova linha adicionado por fgets
     nomes[i][strcspn(nomes[i], "\n")] = '\0';
     
@@ -26,7 +27,7 @@ int main(void) {
     scanf("%d", &estoque[i][1]);
     printf("\n");
     
-    // Limpar o buffer de entrada
+    // Limpar o buffer de entrada**Obs no fim
     while (getchar() != '\n'); // Consome caracteres restantes até nova linha
   }
 
@@ -40,3 +41,14 @@ int main(void) {
   
   return 0;
 }
+
+/* **OBSERVAÇÃO:
+A função strcspn() localiza a primeira ocorrência de um caractere em string1 que pertence ao conjunto de caracteres especificado por string2. Os caracteres nulos não são considerados na procura.
+
+stdin: dispositivo de entrada padrão (geralmente o teclado)
+
+getchar(): É uma função em C que lê 1 caractere do fluxo de entrada padrão (normalmente o teclado quando estamos interagindo com um programa em execução no terminal).
+
+getchar() != '\n'   -> Aqui, getchar() lê um caractere e verifica se não é igual a \n (nova linha). 
+
+O trecho de código while (getchar() != '\n') cria um loop que continua a executar enquanto não for pressionada a tecla Enter. Portanto, o propósito desse loop é consumir todos os caracteres que foram digitados até que seja pressionada a tecla Enter, isso é comumente usado para limpar o buffer de entrada, ou seja, garantir que não haja caracteres indesejados no buffer antes de prosseguir para a próxima parte do código.*/
